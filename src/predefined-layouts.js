ed02ecd221f194f106055b25eb6757f53c7fc95d
xiameng552180 <1832502299@qq.com>
2022-02-17 17:40:29 -0500

    init

---
+++ src/predefined-layouts.js
@@ -0,0 +1,34 @@
+import {
+    ComponentItemConfig,
+    ItemType,
+    LayoutConfig,
+    StackItemConfig,
+} from "golden-layout";
+
+const miniRowConfig = {
+    root: {
+        type: ItemType.row,
+        content: [
+            {
+                type: "component",
+                title: "Title 1st",
+                header: { show: "top" },
+                isClosable: false,
+                componentType: "Content1",
+                width: 10, 
+                componentState: undefined,
+            },
+            {
+                type: "component",
+                title: "I'm wide",
+                header: { show: "top", popout: false },
+                componentType: "Content2",
+                componentState: { abc: 123 },
+            },
+        ],
+    },
+};
+
+export const prefinedLayouts = {
+    miniRow: miniRowConfig,
+}
